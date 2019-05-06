9308c4dc8a1fcd654d65ac3adf1d2a60ef939393
Didier Vojtisek <didier.vojtisek@inria.fr>
2019-05-06 15:24:20 +0200

    initial version of a maven plugin for git-sync

---
+++ git-sync/src/main/java/fr/inria/diverse/git_sync/SyncGitSubmodulesBranchesMojo.java
@@ -0,0 +1,90 @@
+package fr.inria.diverse.git_sync;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Goal which updates a git repo having submodules in order to:
+ * - have a branch corresponding to each available submodule branches
+ * - update heads of the root repo to the head of each submodules
+ *
+ */
+@Mojo( name = "touch", defaultPhase = LifecyclePhase.PROCESS_SOURCES )
+public class SyncGitSubmodulesBranchesMojo
+    extends AbstractMojo
+{
+    /**
+     * Location of the file.
+     */
+    @Parameter( defaultValue = "${project.build.directory}", property = "outputDir", required = true )
+    private File outputDirectory;
+    
+    @Parameter(property="rootGitURL", required = true, readonly = true)
+    private String rootGitURL;
+
+    public void execute()
+        throws MojoExecutionException
+    {
+    	getLog().debug( "###############################################");
+    	getLog().info( "rootGitURL="+rootGitURL);
+        File f = outputDirectory;
+
+        if ( !f.exists() )
+        {
+            f.mkdirs();
+        }
+
+        File touch = new File( f, "touch.txt" );
+
+        FileWriter w = null;
+        try
+        {
+            w = new FileWriter( touch );
+
+            w.write( "touch.txt" );
+        }
+        catch ( IOException e )
+        {
+            throw new MojoExecutionException( "Error creating file " + touch, e );
+        }
+        finally
+        {
+            if ( w != null )
+            {
+                try
+                {
+                    w.close();
+                }
+                catch ( IOException e )
+                {
+                    // ignore
+                }
+            }
+        }
+    }
+}
